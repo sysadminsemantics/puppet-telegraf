55554ecc9822decf45a52e28ef550f5db916c999
Tiago Teodosio <tiago@brandwatch.com>
2018-02-22 12:54:17 +0100

    Telegraf watchdog based on local ports listening. MIT-759

--- manifests/service.pp
+++ manifests/service.pp
@@ -14,5 +14,27 @@ class telegraf::service {
       restart   => 'pkill -HUP telegraf',
       require   => Class['::telegraf::config'],
     }
+
+    if has_key( $::telegraf::inputs, 'tcp_listener') or has_key( $::telegraf::inputs, 'udp_listener') {
+      # setup cronjob to check if ports are open and restart telegraf if not
+      $lsof = "/usr/bin/lsof -n -P"
+      if has_key( $::telegraf::inputs, 'tcp_listener') and has_key( $::telegraf::inputs[tcp_listener], 'service_address') {
+        $tcp = "-i TCP@${::telegraf::inputs[tcp_listener][service_address]}"
+      } else {
+        $tcp = ""
+      }
+      if has_key( $::telegraf::inputs, 'udp_listener') and has_key( $::telegraf::inputs[udp_listener], 'service_address'){
+        $udp = "-i UDP@${::telegraf::inputs[udp_listener][service_address]}"
+      } else {
+        $udp = ""
+      }
+      $restart = "/usr/sbin/service telegraf restart"
+      if "" != $tcp or "" != $udp {
+        Package['lsof'] ->
+        cron { 'ensure telegraf is listening on local ports':
+          command => "${lsof} ${tcp} ${udp} || ${restart} 1>/dev/null 2>/dev/null"
+        }
+      }
+    }
   }
 }
