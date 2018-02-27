0be6761931dd980c6fa99ae9a6095c2e5eeb58c2
Tiago Teodosio <tiago@brandwatch.com>
2018-02-27 19:04:16 +0100

    Fixing variable names in Telegraf watchdog.

--- manifests/service.pp
+++ manifests/service.pp
@@ -18,13 +18,13 @@ class telegraf::service {
     if has_key( $::telegraf::inputs, 'tcp_listener') or has_key( $::telegraf::inputs, 'udp_listener') {
       # setup cronjob to check if ports are open and restart telegraf if not
       if has_key( $::telegraf::inputs, 'tcp_listener') and has_key( $::telegraf::inputs[tcp_listener], 'service_address') {
-        $tcp_port = split( $telegraf_inputs[tcp_listener][service_address], ':')[1]
+        $tcp_port = split( $::telegraf::inputs[tcp_listener][service_address], ':')[1]
         $tcp = "/bin/fuser -s -4 -n tcp ${tcp_port}"
       } else {
         $tcp = "/bin/true"
       }
       if has_key( $::telegraf::inputs, 'udp_listener') and has_key( $::telegraf::inputs[udp_listener], 'service_address') {
-        $udp_port = split( $telegraf_inputs[udp_listener][service_address], ':')[1]
+        $udp_port = split( $::telegraf::inputs[udp_listener][service_address], ':')[1]
         $udp = "/bin/fuser -s -4 -n udp ${udp_port}"
       } else {
         $udp = "/bin/true"
